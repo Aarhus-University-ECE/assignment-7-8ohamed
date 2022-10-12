77de8fc32349b8e807eb086c0680ec9f89742572
8ohamed <114395272+8ohamed@users.noreply.github.com>
2022-10-12 17:09:05 +0200

    Update taylor_sine.h

--- include/taylor_sine.h
+++ include/taylor_sine.h
@@ -1,12 +1,11 @@
 double taylor_sine(double x, int n) {
   assert(n % 2 != 0); // for det skal være ulige tal
+    int t = 0; 
 
   for (int i = 1; i <= n; i += 2) {
     x = x * x;
-    int t = 0; 
     t++; // tælleren
   }
-  int t;
   int result = x / t; // regner hele brøken ud
 
   return -1.0;
