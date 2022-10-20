5c599c9db11425b346275104a53b23483dbee57f
8ohamed <114395272+8ohamed@users.noreply.github.com>
2022-10-20 18:08:59 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
-
+#include <math.h>
+#include "taylor_sine.h"
+#include <stdio.h>
 // Add your test cases for Exercise 1b,c) here
 int main(int argc, char **argv)
 {
+    // sammenligner min sin function med MATH sin function 
+    printf ("%lf\n",taylor_sine(3.14,7));
+    printf ("%lf\n",sin(3.14));
+
+    printf ("%lf\n",taylor_sine(5,7));
+    printf ("%lf\n",sin(5));
+
+
+    printf ("%lf\n",taylor_sine(6,7));
+    printf ("%lf\n",sin(6));
+
     return 0;
 }
