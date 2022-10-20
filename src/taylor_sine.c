aaf4fe7c2e4d994ccec4a7127cb53c502f5ca10c
8ohamed <114395272+8ohamed@users.noreply.github.com>
2022-10-20 18:08:29 +0200

    Update taylor_sine.c

--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -1,7 +1,57 @@
 #include "taylor_sine.h"
-double taylor_sine(double x, int n)
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+double pow(double base, int exponent) {
+    double res = 1;
+        for (int i = 0; i<exponent;i++ ){
+            res = res * base;
+        
+    }
+    return res;
+}
+
+
+
+
+int factorial(int n) {
+if (n==0){
+    return 1;
+}
+
+int res = 1;
+    for (int i =1;i<=n;i++){
+        res = res * i;  
+}
+return res;
+}
+
+
+
+
+bool lige(int x)
 {
-    /* implement your function here */
+    return  (x%2==0);
+}
+
+
+
+
+
+
+
+double taylor_sine(double x, int n)
+ {  
+    double sum =0;
+    int t = 1;
+        for (int i = 0; i < n; i ++) {
+
+
+double fortegn = lig(i)?1:-1;
+
+        sum =sum +fortegn * pow (x,t) / (double)  factorial(t);
+}
+  return sum;
     
-    return -1.0;
 }
